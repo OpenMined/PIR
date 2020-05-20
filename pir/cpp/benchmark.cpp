1a43d0a35682c92ce195a87b823f75a7231422c9
Bogdan Cebere <bogdan.cebere@gmail.com>
2020-05-20 07:43:08 +0300

    add benchmarks

---
+++ pir/cpp/benchmark.cpp
@@ -0,0 +1,115 @@
+#include "benchmark/benchmark.h"
+
+#include "client.h"
+#include "server.h"
+
+namespace pir {
+namespace {
+
+std::vector<std::int64_t> generateDB(std::size_t dbsize) {
+  std::vector<std::int64_t> db(dbsize, 0);
+
+  std::generate(db.begin(), db.end(), [n = 0]() mutable {
+    ++n;
+    return 4 * n;
+  });
+
+  return db;
+}
+
+void BM_ServerLoad(benchmark::State& state) {
+  std::size_t dbsize = state.range(0);
+  auto db = generateDB(dbsize);
+  int64_t elements_processed = 0;
+
+  auto params = PIRParameters::Create(db.size());
+  auto pirdb = PIRDatabase::Create(db, params).ValueOrDie();
+
+  for (auto _ : state) {
+    auto server_ = PIRServer::Create(pirdb, params).ValueOrDie();
+    ::benchmark::DoNotOptimize(server_);
+    elements_processed += dbsize;
+  }
+  state.counters["ElementsProcessed"] = benchmark::Counter(
+      static_cast<double>(elements_processed), benchmark::Counter::kIsRate);
+}  // namespace
+// Range is for the dbsize.
+BENCHMARK(BM_ServerLoad)->RangeMultiplier(10)->Range(10, 10000);
+
+void BM_ClientCreateRequest(benchmark::State& state) {
+  std::size_t dbsize = state.range(0);
+  auto db = generateDB(dbsize);
+
+  auto params = PIRParameters::Create(db.size());
+  auto pirdb = PIRDatabase::Create(db, params).ValueOrDie();
+  auto server_ = PIRServer::Create(pirdb, params).ValueOrDie();
+
+  auto client_ = PIRClient::Create(PIRParameters::Create(dbsize)).ValueOrDie();
+  size_t desiredIndex = dbsize - 1;
+
+  int64_t elements_processed = 0;
+
+  for (auto _ : state) {
+    auto payload = client_->CreateRequest(desiredIndex).ValueOrDie();
+    ::benchmark::DoNotOptimize(payload);
+    elements_processed += dbsize;
+  }
+  state.counters["ElementsProcessed"] = benchmark::Counter(
+      static_cast<double>(elements_processed), benchmark::Counter::kIsRate);
+}  // namespace
+// Range is for the dbsize.
+BENCHMARK(BM_ClientCreateRequest)->RangeMultiplier(10)->Range(10, 10000);
+
+void BM_ServerProcessRequest(benchmark::State& state) {
+  std::size_t dbsize = state.range(0);
+  auto db = generateDB(dbsize);
+
+  auto params = PIRParameters::Create(db.size());
+  auto pirdb = PIRDatabase::Create(db, params).ValueOrDie();
+  auto server_ = PIRServer::Create(pirdb, params).ValueOrDie();
+
+  auto client_ = PIRClient::Create(PIRParameters::Create(dbsize)).ValueOrDie();
+  size_t desiredIndex = dbsize - 1;
+  auto payload = client_->CreateRequest(desiredIndex).ValueOrDie();
+
+  int64_t elements_processed = 0;
+
+  for (auto _ : state) {
+    auto response = server_->ProcessRequest(payload).ValueOrDie();
+    ::benchmark::DoNotOptimize(response);
+    elements_processed += dbsize;
+  }
+  state.counters["ElementsProcessed"] = benchmark::Counter(
+      static_cast<double>(elements_processed), benchmark::Counter::kIsRate);
+}  // namespace
+// Range is for the dbsize.
+BENCHMARK(BM_ServerProcessRequest)->RangeMultiplier(10)->Range(10, 1000);
+
+void BM_ClientProcessResponse(benchmark::State& state) {
+  std::size_t dbsize = state.range(0);
+  auto db = generateDB(dbsize);
+
+  auto params = PIRParameters::Create(db.size());
+  auto pirdb = PIRDatabase::Create(db, params).ValueOrDie();
+  auto server_ = PIRServer::Create(pirdb, params).ValueOrDie();
+
+  auto client_ = PIRClient::Create(PIRParameters::Create(dbsize)).ValueOrDie();
+  size_t desiredIndex = dbsize - 1;
+  auto payload = client_->CreateRequest(desiredIndex).ValueOrDie();
+  auto response = server_->ProcessRequest(payload).ValueOrDie();
+
+  int64_t elements_processed = 0;
+
+  for (auto _ : state) {
+    auto out = client_->ProcessResponse(response).ValueOrDie();
+    ::benchmark::DoNotOptimize(out);
+    elements_processed += dbsize;
+  }
+  state.counters["ElementsProcessed"] = benchmark::Counter(
+      static_cast<double>(elements_processed), benchmark::Counter::kIsRate);
+}  // namespace
+// Range is for the dbsize.
+BENCHMARK(BM_ClientProcessResponse)->RangeMultiplier(10)->Range(10, 1000);
+
+}  // namespace
+}  // namespace pir
