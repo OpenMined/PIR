8d17b3d485d72f3f978a3c39faf58ff2de3a62ba
Kareem Shehata <kareem@shehata.ca>
2020-05-24 21:36:36 +0800

    Adding utils library missed on previous commit

---
+++ pir/cpp/utils.cpp
@@ -0,0 +1,32 @@
+#include "utils.h"
+
+namespace pir {
+
+using std::vector;
+
+vector<uint32_t> generate_galois_elts(uint64_t N) {
+  const size_t logN = ceil_log2(N);
+  vector<uint32_t> galois_elts(logN);
+  for (size_t i = 0; i < logN; ++i) {
+    galois_elts[i] = (N >> i) + 1;
+  }
+  return galois_elts;
+}
+
+uint32_t ceil_log2(uint32_t v) {
+  static const int MultiplyDeBruijnBitPosition[32] = {
+      0,  1,  28, 2,  29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4,  8,
+      31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6,  11, 5,  10, 9};
+
+  --v;
+  v |= v >> 1;
+  v |= v >> 2;
+  v |= v >> 4;
+  v |= v >> 8;
+  v |= v >> 16;
+  ++v;
+
+  return MultiplyDeBruijnBitPosition[(uint32_t)(v * 0x077CB531U) >> 27];
+}
+
+}  // namespace pir
---
+++ pir/cpp/utils.h
@@ -0,0 +1,61 @@
+//
+// Copyright 2020 the authors listed in CONTRIBUTORS.md
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#ifndef PIR_UTILS_H_
+#define PIR_UTILS_H_
+
+#include <cstdint>
+#include <vector>
+
+namespace pir {
+
+// Utility function to generate Galois elements needed for Oblivious Expansion.
+std::vector<uint32_t> generate_galois_elts(uint64_t N);
+
+// Utility function to find the next highest power of 2 of a given number.
+template <typename t>
+t next_power_two(t n) {
+  if (n == 0) return 1;
+  --n;
+  for (int i = 1; i < sizeof(n) * 8; i = i << 1) {
+    n |= n >> i;
+  }
+  return n + 1;
+}
+
+// Utility function to find the log base 2 of v rounded up.
+uint32_t ceil_log2(uint32_t v);
+
+}  // namespace pir
+
+namespace private_join_and_compute {
+// Really don't know why this isn't included with private_join_and_compute
+
+// Run a command that returns a util::Status.  If the called code returns an
+// error status, return that status up out of this method too.
+//
+// Example:
+//   RETURN_IF_ERROR(DoThings(4));
+#define RETURN_IF_ERROR(expr)                                                \
+  do {                                                                       \
+    /* Using _status below to avoid capture problems if expr is "status". */ \
+    const Status _status = (expr);                                           \
+    if (!_status.ok()) return _status;                                       \
+  } while (0)
+
+}  // namespace private_join_and_compute
+
+#endif  // PIR_UTILS_H_
