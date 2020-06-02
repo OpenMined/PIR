8d29b258c4c8d3f1b4e0a3825fba5bd3a9952d21
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-06-02 18:05:45 +0300

    fix serialization tests

--- pir/cpp/serialization_test.cpp
+++ pir/cpp/serialization_test.cpp
@@ -16,8 +16,12 @@
 
 #include "pir/cpp/serialization.h"
 
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "pir/cpp/context.h"
 #include "pir/cpp/utils.h"
+#include "seal/seal.h"
+#include "util/statusor.h"
 
 namespace pir {
 
@@ -27,6 +31,8 @@ using std::make_tuple;
 using std::make_unique;
 using std::tuple;
 
+using ::testing::ElementsAreArray;
+
 class PIRSerializationTest : public ::testing::Test {
  protected:
   static constexpr std::size_t DB_SIZE = 100;
@@ -34,46 +40,51 @@ class PIRSerializationTest : public ::testing::Test {
 
   void SetUpDB(size_t dbsize) {
     pir_params_ = PIRParameters::Create(dbsize);
-    client_ = PIRClient::Create(pir_params_).ValueOrDie();
+    context_ = std::move(PIRContext::Create(pir_params_).ValueOrDie());
 
-    ASSERT_TRUE(client_ != nullptr);
+    auto keygen_ =
+        std::make_unique<seal::KeyGenerator>(context_->SEALContext());
+    encryptor_ = std::make_shared<seal::Encryptor>(context_->SEALContext(),
+                                                   keygen_->public_key());
+    decryptor_ = std::make_shared<seal::Decryptor>(context_->SEALContext(),
+                                                   keygen_->secret_key());
   }
 
-  std::shared_ptr<seal::Decryptor> Decryptor() { return client_->decryptor_; }
-  std::shared_ptr<seal::Encryptor> Encryptor() { return client_->encryptor_; }
-
   std::shared_ptr<PIRParameters> pir_params_;
-  std::unique_ptr<PIRClient> client_;
+  std::shared_ptr<PIRContext> context_;
+  std::shared_ptr<seal::Encryptor> encryptor_;
+  std::shared_ptr<seal::Decryptor> decryptor_;
 };
 
 TEST_F(PIRSerializationTest, TestResponseSerialization) {
   int64_t value = 987654321;
-  Plaintext pt;
-  Context()->Encoder()->encode(value, pt);
+  Plaintext pt, reloaded_pt;
+  context_->Encoder()->encode(value, pt);
   vector<Ciphertext> ct(1);
-  Encryptor()->encrypt(pt, ct[0]);
+  encryptor_->encrypt(pt, ct[0]);
 
   Response response_proto;
-  SaveCiphertexts(ct, response_proto.mutable_response());
+  SaveCiphertexts(ct, response_proto.mutable_reply());
 
   auto reloaded_or =
-      LoadCiphertexts(Context()->SEALContext(), response_proto.response());
+      LoadCiphertexts(context_->SEALContext(), response_proto.reply());
   ASSERT_TRUE(reloaded_or.ok())
       << "Status is: " << reloaded_or.status().ToString();
 
   auto reloaded = reloaded_or.ValueOrDie();
   ASSERT_EQ(reloaded.size(), 1);
-  EXPECT_THAT(reloaded, ElementsAreArray(ct));
+  decryptor_->decrypt(reloaded[0], reloaded_pt);
+  EXPECT_THAT(reloaded_pt, pt);
 }
 
 TEST_F(PIRSerializationTest, TestRequestSerialization) {
   int64_t value = 987654321;
-  Plaintext pt;
-  Context()->Encoder()->encode(value, pt);
+  Plaintext pt, reloaded_pt;
+  context_->Encoder()->encode(value, pt);
   vector<Ciphertext> ct(1);
-  Encryptor()->encrypt(pt, ct[0]);
+  encryptor_->encrypt(pt, ct[0]);
 
-  auto keygen_ = make_unique<KeyGenerator>(Context()->SEALContext());
+  auto keygen_ = make_unique<KeyGenerator>(context_->SEALContext());
   auto elts = generate_galois_elts(DEFAULT_POLY_MODULUS_DEGREE);
   GaloisKeys gal_keys = keygen_->galois_keys_local(elts);
 
@@ -82,20 +93,21 @@ TEST_F(PIRSerializationTest, TestRequestSerialization) {
   SEALSerialize<GaloisKeys>(gal_keys, request_proto.mutable_keys());
 
   auto request_or =
-      LoadCiphertexts(Context()->SEALContext(), request_proto.query());
+      LoadCiphertexts(context_->SEALContext(), request_proto.query());
   ASSERT_TRUE(request_or.ok())
       << "Status is: " << request_or.status().ToString();
 
   auto request = request_or.ValueOrDie();
-  auto keys_r = SEALDeserialize<GaloisKeys>(Context()->SEALContext(),
-                                            request_proto.keys())
-                    .ValueOrDie();
+  auto keys_or = SEALDeserialize<GaloisKeys>(context_->SEALContext(),
+                                             request_proto.keys());
   ASSERT_TRUE(keys_or.ok()) << "Status is: " << keys_or.status().ToString();
   auto keys = keys_or.ValueOrDie();
 
   ASSERT_EQ(request.size(), 1);
-  EXPECT_THAT(reloaded, ElementsAreArray(ct));
-  ASSERT_THAT(keys, ElementsAreArray(elts));
+  decryptor_->decrypt(request[0], reloaded_pt);
+  ASSERT_EQ(reloaded_pt, pt);
+
+  for (auto &elt : elts) ASSERT_TRUE(keys.has_key(elt));
 }
 
 }  // namespace pir
