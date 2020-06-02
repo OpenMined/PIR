f64c83502c6ca28d2dda93c8c2964f5afb8539b8
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-06-02 19:41:15 +0300

    add Bazel linters

---
+++ .github/workflows/scripts/lint_bazel.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+go get github.com/bazelbuild/buildtools/buildifier
+
+# Lint files (all BUILD) inplace.
+find ./pir/  \( -iname BUILD \) | xargs buildifier
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+# Print changes.
+git diff
+# Already well formated if 'git diff' doesn't output anything.
+! ( git diff |  grep -q ^ ) || exit 1
